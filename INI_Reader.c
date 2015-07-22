bfc17ad781b016108ffe30f06f7d82b23dbf53b1
Gavin <garlan0228@yahoo.com>
2015-07-22 15:04:56 -0400

    Update INI_Reader.c
    merged with improved.c

--- INI_Reader.c
+++ INI_Reader.c
@@ -1,30 +1,29 @@
 #include "INI_Reader.h"
 #include <stdlib.h>
 
-
-
-unsigned int ini_pos=0;
-unsigned int ini_len=0;
-int ini_eof=0;
+unsigned int ini_pos = 0;
+unsigned int ini_len = 0;
+int ini_eof = 0;
 static char line_buffer[50];
-int ini_read_line(char* ini,char* line_buf)
+static int line_buffer_pos=0;
+int ini_read_line(char* ini, char* line_buf)
 {
-	int  line_len=0;
+	int  line_len = 0;
 	char* line_start;
 	//reach the end of the file
-	if(ini_pos==ini_len)
+	if (ini_pos == ini_len)
 		return 0;
 
-	line_start=ini+ini_pos;
-	while(ini_pos<ini_len)
+	line_start = ini + ini_pos;
+	while (ini_pos<ini_len)
 	{
-		if(ini[ini_pos]=='\n')
+		if (ini[ini_pos] == '\n')
 		{
 			ini_pos++;
 			line_len++;
 			break;
 		}
-		else if(ini[ini_pos]=='\r')
+		else if (ini[ini_pos] == '\r')
 		{
 			ini_pos++;
 			if (ini[ini_pos] == '\n')
@@ -35,19 +34,21 @@ int ini_read_line(char* ini,char* line_buf)
 		}
 		line_len++;
 		ini_pos++;
+		line_buffer_pos = 0;
 	}
 
-	strncpy(line_buf,line_start,line_len);
+	strncpy(line_buf, line_start, line_len);
 	line_buf[line_len] = 0;
 	return 1;
 }
-INI_Section parse_ini(char* ini,int page)
+INI_Section parse_ini(char* ini, int page)
 {
-	ini_pos=0;
-	ini_eof=0;
-	ini_len=strlen(ini);
+	ini_pos = 0;
+	ini_eof = 0;
+	ini_len = strlen(ini);
 
 }
+
 int is_kv_line(char * line)
 {
 	return strchr(line, '=') != NULL;
@@ -56,127 +57,67 @@ int is_section_line(char* line)
 {
 	return strchr(line, '[') != NULL;
 }
-int get_key_value(char*line,INI_KeyValue *kv)
-{
-	int pos=0;
-	char c;
-	int copy_key,copy_val,copy_str,key_pos,val_pos;
-	//start to copy key
-  copy_key=1;
-	copy_val=0;
-	key_pos=0;
-	val_pos=0;
-	c=line_buffer[0];
-	
-	while(c!=0)
-	{
-		if(c=='#')
-		{
-			break;
-		}
-		else if(c=='=')
-		{
-			copy_key=0;
-			copy_val=1;
-			//stop copying key
-			kv->key[key_pos]=0;
-		}
-		else if(c==' ')
-		{
-			if(copy_key)
-			{
-				continue;
-			}
-			//start copying the value,
-			else if(copy_val)
-			{
-				kv->val_str[val_pos]=line[pos];
-				val_pos++;
-			}
-		}
-		
-		else if(c=='"')
-		{
-			
-		}
-		else
-		{
-			if(copy_key)
-			{
-				kv->key[key_pos]=line[pos];
-				key_pos++;
-			}
-			else if(copy_val)
-			{
-				kv->val_str[val_pos]=line[pos];
-				val_pos++;
-			}
-		}
-		pos++;
-		c=line_buffer[pos];
-	}
-	if(pos==0)
-	{
-		kv=NULL;
-	} 
-	kv->val_str[val_pos]=0;
-	
-	return 1;
-}
-int get_section_name(char* line, char* name)
-{
-	int pos=0;
-	char c;
-	int open_brac,is_section,copy_str,copy_pos;
 
-	open_brac=0;
-	is_section=0;
-	copy_str=0;
-	copy_pos=0;
-	c=line_buffer[0];
-	 while(c!=0)
-	 {
-		 if(c==' ')
-		 {
-		 }
-		 if(c=='#')
-		 {
-			 break;
-		 }
-		 else if(c=='[')
-		 {
-			 open_brac=1;
-			 is_section=1;
-			 copy_str=1;
-		 }
-		 else if(c==']')
-		 {
-			 open_brac=0;
-			 copy_str=0;
-			 name[copy_pos]=0;
-		 }
-		 //legal character
-		 else
-		 {
-			 if(copy_str==1)
-			 {
-			 name[copy_pos]=line[pos];
-			 copy_pos++;
-			 }
-		 }
-		 
-		 pos++;
-		 c=line_buffer[pos];
-	 }
-	 //the end of a line
-	 //unclosed bracket
-	 if(open_brac==1)
-	 {
-		 return 0;
-	 }
-	 return 1;
+int match_lb()
+{
+	return line_buffer[line_buffer_pos] == '['&&line_buffer_pos++;
 }
-INI_Section * get_ini_section_by_name(INI_Section** sections,int section_num, char* section_name)
+int match_rb()
+{
+	return line_buffer[line_buffer_pos] == ']'&&line_buffer_pos++;
+}
+int match_eq()
+{
+	return line_buffer[line_buffer_pos] == '='&&line_buffer_pos++;
+}
+int is_legal_key_char(char c);
+char see_next();
+int extract_str(char* dest)
+{
+	char c = line_buffer[line_buffer_pos];
+	int pos = 0;
+	is_legal_key_char(c);
+	dest[pos] = c;
+	pos++;
+	c = see_next();
+	while (is_legal_key_char(c))
+	{
+		dest[pos] = c;
+		pos++;
+		line_buffer_pos++;
+		c = see_next();
+	}
+	dest[pos] = 0;
+	line_buffer_pos++;
+}
+char see_next()
+{
+	return *(line_buffer + line_buffer_pos + 1);
+}
+void parse_section(char* string)
+{
+
+	match_lb();
+	extract_str(string);
+	match_rb();
+	printf(" %s ", string);
+
+}
+void parse_kv(char *key, char* value)
+{
+	extract_str(key);
+	match_eq();
+	extract_str(value);
+}
+int is_legal_key_char(char c)
+{
+	if (c != '\n'&&c != '\r'&&c != '='&&c != ']')
+		return 1;
+	else
+		return 0;
+}
+
+INI_Section * get_ini_section_by_name(INI_Section** sections, int section_num, char* section_name)
 {
 	int i;
 	for (i = 0; i < section_num; i++)
@@ -188,33 +129,31 @@ INI_Section * get_ini_section_by_name(INI_Section** sections,int section_num, ch
 	}
 	return NULL;
 }
-int ini_load(char* ini,INI_Section ** sections,int *section_num)
+int ini_load(char* ini, INI_Section ** sections, int *section_num)
 {
-	ini_pos=0;
-	ini_eof=0;
+	ini_pos = 0;
+	ini_eof = 0;
 	INI_KeyValue *kv;
 	int kv_size = 0;
-	ini_len=strlen(ini);
+	ini_len = strlen(ini);
 	*sections = ini_malloc(sizeof(void*)*MAX_SECTION_SIZE);
 	int sec_num = 0;
-	while(ini_read_line(ini,line_buffer))
+	while (ini_read_line(ini, line_buffer))
 	{
-		if(is_section_line(line_buffer))
+		if (is_section_line(line_buffer))
 		{
 			sections[sec_num] = ini_malloc(sizeof(INI_Section));
-			get_section_name(line_buffer,sections[sec_num]->name);
+			parse_section(sections[sec_num]->name);
 			sections[sec_num]->kv_size = 0;
 			sec_num++;
-			
-		}
-		else if(is_kv_line(line_buffer))
-		{
-			
-			kv=ini_malloc(sizeof(INI_KeyValue));
-			get_key_value(line_buffer, kv);
-			sections[sec_num - 1]->kv[sections[sec_num - 1]->kv_size] = kv;
 
-			sections[sec_num- 1]->kv_size++;
+		}
+		else if (is_kv_line(line_buffer))
+		{
+			kv = ini_malloc(sizeof(INI_KeyValue));
+			parse_kv(kv->key,kv->val_str);
+			sections[sec_num - 1]->kv[sections[sec_num - 1]->kv_size] = kv;
+			sections[sec_num - 1]->kv_size++;
 		}
 
 	}
@@ -233,4 +172,5 @@ int ini_unload(INI_Section ** sections, int section_num)
 		}
 		ini_free(sections[i]);
 	}
+	ini_free(*sections);
 }
