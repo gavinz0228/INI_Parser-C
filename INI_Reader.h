d479dad0f6f8175e91d954cf6c69d55736ec827e
Gavin <garlan0228@yahoo.com>
2015-07-22 22:07:13 -0400

    Update INI_Reader.h

--- INI_Reader.h
+++ INI_Reader.h
@@ -2,26 +2,31 @@
 #ifndef INI_READER_H
 #define INI_READER_H
 
-#define MAX_SECTION_SIZE			50
+#define MAX_SECTION_SIZE			100
+#define LINE_BUFFER_SIZE			100
+#define MAX_KEY_VALUE				50
 #define ini_malloc(size)			malloc(size)
 #define ini_free(ptr)				free(ptr)
 typedef struct
 {
 	char key[50];
-	char val_str[50];
-	long val_int;
-	int val_type;
+	char value[50];
 }INI_KeyValue;
 
 typedef struct
 {
 	char name[50];
-	INI_KeyValue* kv[50];
+	INI_KeyValue* kv[MAX_KEY_VALUE];
 	int kv_size;
 }INI_Section;
+typedef struct
+{
+	INI_Section* sections[MAX_SECTION_SIZE];
+	int size; 
+}INI_File;
 
-int ini_load(char* ini, INI_Section ** sections, int *section_num);
+int ini_load(char* ini, INI_File *ini_file);
 INI_Section * get_ini_section_by_name(INI_Section** sections, int section_num, char* section_name);
-int ini_unload(INI_Section ** sections, int section_num);
+int ini_unload(INI_File *ini);
 
 #endif
